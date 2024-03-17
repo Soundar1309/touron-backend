7673c6e6d9f2058a86dc586b6be704d57922e54c
Soundar Rajan <87408909+Soundar1309@users.noreply.github.com>
2024-03-17 13:41:22 +0530

    Update server.js

--- server.js
+++ server.js
@@ -3,8 +3,16 @@ const cors = require("cors");
 require("dotenv").config();
 const connectDB = require("./config/db");
 
+const corsObj = {
+  origin: '*',
+  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+  preflightContinue: false,
+  optionsSuccessStatus: 200,
+};
+//using the cors obj
+
 const app = express();
-app.use(cors());
+app.use(cors(corsObj));
 connectDB();
 app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ extended: false, limit: "50mb" }));
